0297ccf6fa0ea6ca3017f7f1c186bd53d57b7d3a
IMTR0J4N <jjorgebeau.pro@gmail.com>
2025-02-16 12:21:32 +0100

    Enhance MarkdownParser to display a message when no content is available

--- src/components/MarkdownParser.tsx
+++ src/components/MarkdownParser.tsx
@@ -7,7 +7,7 @@ import { parseMarkdown } from '@lib/md_parser';
 
 const MarkdownParser = ({ markdown, classes = {} }: MarkdownParserProps) => {
   const content = parseMarkdown(markdown, classes);
-  return <div className={`${classes.container ?? ''}`}>{content}</div>;
+  return markdown.length > 0 ? <div className='text-white'>{content}</div> : <div className='w-full flex items-center justify-center'> <h2 className='text-white text-lg'>Aucun contenu disponible</h2> </div>;
 };
 
 export default MarkdownParser;
